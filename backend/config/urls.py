ca26728195af33bfaea0b6e8aebf37ebce3ed847
AxelleBdd <axbaudinaud@gmail.com>
2026-01-13 14:34:57 +0100

    wip: creation authentication

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,7 +22,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("admin/", admin.site.urls),
     path("api/categories/", include("categories.urls")),
     path("api/products/", include("products.urls")),
