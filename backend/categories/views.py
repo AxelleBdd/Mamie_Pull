1ec4cd8b981f51290161003685b45fbe65989bd8
AxelleBdd <axbaudinaud@gmail.com>
2026-01-13 14:16:33 +0100

    add : products and categories endpoints

--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,75 +1,101 @@
 import json
-from django.http import HttpResponseNotAllowed, JsonResponse
-from django.shortcuts import render
-from .models import Category
 
-#Category views
+from .models import Category
+from django.http import HttpResponseNotAllowed, JsonResponse
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
+
 def category_api(request, category_id=None):
     if request.method == "GET":
         if category_id is None:
-            categories = Category.objects.all()
-            data = [
-                {"id": category.id, "name": category.name}
-                for category in categories
-            ]
-            return JsonResponse(data, safe=False)
+            return get_categories()
         else:
-            try:
-                category = Category.objects.get(id=category_id)
-            except Category.DoesNotExist:
-                return JsonResponse({"error": "Not found"}, status=404)
-
-            return JsonResponse({
-                "id": category.id,
-                "name": category.name
-            })
+            return get_category(category_id)
     
     elif request.method == "POST":
-        body = json.loads(request.body)
-        category = Category.objects.create(
-            name=body.get("name")
-        )
-        return JsonResponse({
-            "id": category.id,
-            "name": category.name
-        }, status=201)
-
+        return create_category(request)
+    
     elif request.method == "PUT":
-        if category_id is None:
-            return JsonResponse(
-                {"error": "Category ID required"},
-                status=400
-            )
-
-        body = json.loads(request.body)
-        try:
-            category = Category.objects.get(id=category_id)
-        except Category.DoesNotExist:
-            return JsonResponse({"error": "Not found"}, status=404)
-
-        category.name = body.get("name", category.name)
-        category.save()
-
-        return JsonResponse({
-            "id": category.id,
-            "name": category.name
-        })
+        return update_category(request, category_id)
     
     elif request.method == "DELETE":
-        if category_id is None:
-            return JsonResponse(
-                {"error": "Category ID required"},
-                status=400
-            )
-
-        try:
-            category = Category.objects.get(id=category_id)
-        except Category.DoesNotExist:
-            return JsonResponse({"error": "Not found"}, status=404)
-
-        category.delete()
-        return JsonResponse({}, status=204)
-
+        return delete_category(category_id)
+    
     return HttpResponseNotAllowed(["GET", "POST", "PUT", "DELETE"])
 
-# Create your views here.
+#GET all categories
+def get_categories():
+    categories = Category.objects.all()
+    data = [
+        {"id": category.id, "name": category.name}
+        for category in categories
+    ]
+    return JsonResponse(data, safe=False)
+
+#GET category by id
+def get_category(category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return JsonResponse({"error": "Not found"}, status=404)
+    
+    return JsonResponse({
+        "id": category.id,
+        "name": category.name
+    })
+
+# POST category
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_category(request):
+    body = json.loads(request.body)
+    category = Category.objects.create(
+        name=body.get("name")
+    )
+    return JsonResponse({
+        "id": category.id,
+        "name": category.name
+    }, status=201)
+
+#PUT catgory
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def update_category(request, category_id):
+    if category_id is None:
+        return JsonResponse(
+            {"error": "Category ID required"},
+            status=400
+        )
+    
+    body = json.loads(request.body)
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return JsonResponse({"error": "Not found"}, status=404)
+    
+    category.name = body.get("name", category.name)
+    category.save()
+    
+    return JsonResponse({
+        "id": category.id,
+        "name": category.name
+    })
+
+#DELETE category
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_category(category_id):
+    if category_id is None:
+        return JsonResponse(
+            {"error": "Category ID required"},
+            status=400
+        )
+    
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return JsonResponse({"error": "Not found"}, status=404)
+    
+    category.delete()
+    return JsonResponse({}, status=204)
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,87 +1,115 @@
 import json
+
+from .models import Product
 from django.http import HttpResponseNotAllowed, JsonResponse
-from django.shortcuts import render
 
-from products.models import Product
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
 
-#Products views
 def product_api(request, product_id=None):
-    if request.method == 'GET' :
-        if product_id is None: #GET all
-            products = Product.objects.all()
-            data = [
-                {"id": product.id, "title": product.title, "description": product.description, "category": product.category, "image": product.image}
-                for product in products
-            ]
-            return JsonResponse(data, safe=False)
+    if request.method == "GET":
+        if product_id is None:
+            return get_products()
         else:
-            try: #GET product_id
-                product = Product.objects.get(id=product_id)
-            except Product.DoesNotExist:
-                return JsonResponse({"error": "Not found"}, status=404)
-
-            return JsonResponse({
-                "id": product.id,
-                "title": product.title,
-                "description": product.description, 
-                "category": product.category, 
-                "image": product.image
-            })
-        
+            return get_product(product_id)
+    
     elif request.method == "POST":
         body = json.loads(request.body)
-        product = Product.objects.create(
-            name=body.get("name")
-        )
-        return JsonResponse({
-            "id": product.id,
-            "title": product.title,
-            "description": product.description, 
-            "category": product.category, 
-            "image": product.image
-        }, status=201)
+        return create_product(body)
     
     elif request.method == "PUT":
-        if product_id is None:
-            return JsonResponse(
-                {"error": "Product ID required"},
-                status=400
-            )
-
         body = json.loads(request.body)
-        try:
-            product = Product.objects.get(id=product_id)
-        except Product.DoesNotExist:
-            return JsonResponse({"error": "Not found"}, status=404)
-
-        product.title = body.get("title", product.title)
-        product.description = body.get("description", product.description)
-        product.category = body.get("category", product.category)
-        product.image = body.get("image", product.image)
-        product.save()
-
-        return JsonResponse({
-            "id": product.id,
-            "title": product.title,
-            "description": product.description, 
-            "category": product.category, 
-            "image": product.image
-        })
+        return update_product(product_id, body)
     
     elif request.method == "DELETE":
-        if product_id is None:
-            return JsonResponse(
-                {"error": "Product ID required"},
-                status=400
-            )
-
-        try:
-            product = Product.objects.get(id=product_id)
-        except Product.DoesNotExist:
-            return JsonResponse({"error": "Not found"}, status=404)
-
-        product.delete()
-        return JsonResponse({}, status=204)
-
+        return delete_product(product_id)
+    
     return HttpResponseNotAllowed(["GET", "POST", "PUT", "DELETE"])
 
+#GET all products
+def get_products():
+    products = Product.objects.all()
+    data = [
+        {
+            "id": product.id,
+            "title": product.title,
+            "description": product.description,
+            "category": product.category,
+            "image": product.image
+        }
+        for product in products
+    ]
+    return JsonResponse(data, safe=False)
+
+#GET product by id
+def get_product(product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return JsonResponse({"error": "Not found"}, status=404)
+    
+    return JsonResponse({
+        "id": product.id,
+        "title": product.title,
+        "description": product.description,
+        "category": product.category,
+        "image": product.image
+    })
+
+# POST product
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_product(body):
+    product = Product.objects.create(
+        name=body.get("name")
+    )
+    return JsonResponse({
+        "id": product.id,
+        "title": product.title,
+        "description": product.description,
+        "category": product.category,
+        "image": product.image
+    }, status=201)
+
+#PUT product
+def update_product(product_id, body):
+    if product_id is None:
+        return JsonResponse(
+            {"error": "Product ID required"},
+            status=400
+        )
+    
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return JsonResponse({"error": "Not found"}, status=404)
+    
+    product.title = body.get("title", product.title)
+    product.description = body.get("description", product.description)
+    product.category = body.get("category", product.category)
+    product.image = body.get("image", product.image)
+    product.save()
+    
+    return JsonResponse({
+        "id": product.id,
+        "title": product.title,
+        "description": product.description,
+        "category": product.category,
+        "image": product.image
+    })
+
+#DELETE product
+def delete_product(product_id):
+    if product_id is None:
+        return JsonResponse(
+            {"error": "Product ID required"},
+            status=400
+        )
+    
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return JsonResponse({"error": "Not found"}, status=404)
+    
+    product.delete()
+    return JsonResponse({}, status=204)
