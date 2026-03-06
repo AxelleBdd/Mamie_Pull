b02cef626a6ed07e87a4c7efdc874f928896f5aa
AxelleBdd <axbaudinaud@gmail.com>
2026-03-06 11:41:14 +0100

    tests: products routes

--- backend/products/tests.py
+++ backend/products/tests.py
@@ -5,12 +5,42 @@ from products.models import Category
 # Create your tests here.
 class ProductTestCase(TestCase):
     def setUp(self):
-        Category.objects.create(name="Test Category")
-        Product.objects.create(title="Test Product 1", description="Description for product 1", category_id=1)
-        Product.objects.create(title="Test Product 2", description="Description for product 2", category_id=1)
+        category = Category.objects.create(name="Test Category")
+        Product.objects.create(title="Test Product 1", description="Description for product 1", category=category)
+        Product.objects.create(title="Test Product 2", description="Description for product 2", category=category)
 
+    # POST
     def test_product_creation(self):
         product1 = Product.objects.get(title="Test Product 1")
         product2 = Product.objects.get(title="Test Product 2")
         self.assertEqual(product1.description, "Description for product 1")
         self.assertEqual(product2.description, "Description for product 2")
+
+    #GET (by category, by id, all)
+    def test_product_get_all(self):
+        products = Product.objects.all()
+        self.assertEqual(len(products), 2)
+
+    def test_product_get_product_by_id(self):
+        product = Product.objects.get(title="Test Product 1")
+        self.assertEqual(product.description, "Description for product 1")
+    
+    def test_product_get_products_by_category(self):
+        category = Category.objects.get(name="Test Category")
+        products = Product.objects.filter(category=category)
+        self.assertEqual(len(products), 2)
+
+    #PUT
+    def test_product_update(self):
+        product = Product.objects.get(title="Test Product 1")
+        product.description = "Updated description for product 1"
+        product.save()
+        updated_product = Product.objects.get(title="Test Product 1")
+        self.assertEqual(updated_product.description, "Updated description for product 1")
+
+    #DELETE
+    def test_product_delete(self):
+        product = Product.objects.get(title="Test Product 1")
+        product.delete()
+        with self.assertRaises(Product.DoesNotExist):
+            Product.objects.get(title="Test Product 1")
